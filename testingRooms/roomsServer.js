4c5a2eb5cc39089fa710531f52c6fabc6ec372b6
harrycummins <115977764+harrycummins@users.noreply.github.com>
2024-11-21 22:34:05 +0000

    linked home page and rooms page

--- testingRooms/roomsServer.js
+++ testingRooms/roomsServer.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = 3000;
 
 const app = express()
 app.use(bodyParser.json())
