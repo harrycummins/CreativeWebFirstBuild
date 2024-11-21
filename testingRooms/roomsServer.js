9381df54f4ff6f6bda74da4c97d802215a24c889
harrycummins <hazcummins@icloud.com>
2024-11-21 21:07:41 +0000

    first push

---
+++ testingRooms/roomsServer.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = 3000;
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors());
+
+console.log("listening on port")
+
+const rooms = {} //mongo DB will be added wehn needed 
+
+
+app.post('/create-room', (req,res)=>{
+    const roomkey = Math.random().toString(36).substring(2,8).ToupperCase()
+    rooms[roomKey] = {users: []}
+    res.json({ roomkey })
+})
+
+app.post('/join-room', (req, res) => {
+    const roomkey = { roomKey , username} = req.body
+
+    if(!rooms[roomKey]){
+        return res.status(404).send('Room not found')
+    }
+    
+    rooms [roomKey].users.push(username)
+    res.json({message:'Room joined ', users: rooms[roomKey].users})
+})
+
+app.get('/room:roomKey', (req,res) =>{
+    const { roomKey} = req.params
+
+    if (!rooms[roomKey]){
+        return res.json({message:'Incorrect code', users:rooms[roomKey].users})
+    }
+})
